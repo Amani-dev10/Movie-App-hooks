8b56d30b80d26b5dbe331c54a1291f4dbcb6f632
Amani-dev10 <benmansourameni22@gmail.com>
2022-05-16 21:06:39 +0200

    Movie App hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+import { useState } from 'react';
+import { moviesData } from './data';
+import MovieList from './components/MovieList';
+import Search from './components/Search';
+import AddMovie from './components/AddMovie';
 
 function App() {
+  const [movies, setMovies] = useState(moviesData);
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(1);
+  
+  const handleAdd= (newMovie) => setMovies([...movies,newMovie]);
+
+  const handleText = (e) => setText(e.target.value);
+  
+  const handleRating = (x) => setRating(x);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search rating={rating} text={text} handleText={handleText} handleRating={handleRating} />
+      <MovieList movies={movies.filter(el =>el.name.toLowerCase().includes(text.toLowerCase()) 
+      && (el.rating >= rating))} />
+      <AddMovie add={handleAdd} />
+  
     </div>
   );
 }
